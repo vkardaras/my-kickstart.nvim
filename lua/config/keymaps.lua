b3d307c1b02d3946fc8557695023bfb7db386a5c
Vasilis Kardaras <vasiliskardaras@gmail.com>
2024-10-01 17:08:36 +0300

    keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,14 +10,15 @@ keymap.set("n", "<leader>ww", ":w<CR>") -- save
 keymap.set("n", "gx", ":!open <c-r><c-a><CR>") -- open URL under cursor
 
 -- Split window management
-keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
+keymap.set("n", "<leader>|", "<C-w>v") -- split window vertically
+keymap.set("n", "<leader>-", "<C-w>s") -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width
 keymap.set("n", "<leader>sx", ":close<CR>") -- close split window
 keymap.set("n", "<leader>sj", "<C-w>-") -- make split window height shorter
 keymap.set("n", "<leader>sk", "<C-w>+") -- make split windows height taller
 keymap.set("n", "<leader>sl", "<C-w>>5") -- make split windows width bigger 
 keymap.set("n", "<leader>sh", "<C-w><5") -- make split windows width smaller
+keymap.set("n", "<leader>o", "<C-w>o") -- close all but the current window
 
 -- Better window navigation
 keymap.set("n", "<C-h>", ":wincmd h<cr>", { desc = "Move focus to the left window" })
@@ -30,6 +31,12 @@ keymap.set("n", "<leader>to", ":tabnew<CR>") -- open a new tab
 keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close a tab
 keymap.set("n", "<leader>tn", ":tabn<CR>") -- next tab
 keymap.set("n", "<leader>tp", ":tabp<CR>") -- previous tab
+
+-- Buffer management
+keymap.set("n", "<leader>ls", ":ls<CR>") -- list buffers
+keymap.set("n", "<leader>bn", ":bn<CR>") -- next buffer
+keymap.set("n", "<leader>bp", ":bp<CR>") -- previous buffer
+keymap.set("n", "<leader>bd", ":bd<CR>") -- close buffer
 
 -- Diff keymaps
 keymap.set("n", "<leader>cc", ":diffput<CR>") -- put diff from current to other during diff
