46ca70af26b065ba17c73a115d52c2d4640f6428
Vasilis Kardaras <vasiliskardaras@gmail.com>
2024-10-29 11:08:06 +0200

    xml config

---
+++ ftplugin/xml.lua
@@ -0,0 +1,4 @@
+-- Java specific settings
+vim.opt.shiftwidth = 4 -- number of spaces inserted for each indentation level
+vim.opt.tabstop = 4 -- number of spaces inserted for tab character
+vim.opt.softtabstop = 4 -- number of spaces inserted for <Tab> key
